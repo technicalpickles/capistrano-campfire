c2ebf077619090ce42eea8eab3a0d84d2bf5af2f
Josh Nichols <josh@technicalpickles.com>
2012-10-15 11:57:28 -0400

    Initial implementation of speaking to multiple rooms

--- lib/capistrano/campfire.rb
+++ lib/capistrano/campfire.rb
@@ -3,6 +3,33 @@ require 'tinder'
 
 module Capistrano
   module Campfire
+    class RoomCollection
+      def initialize(campfire_options)
+        rooms_options = campfire_options[:rooms]
+
+        @rooms = rooms_options.map do |room_options|
+          account = room_options[:account] || campfire_options[:account]
+          token = room_options[:token] || campfire_options[:token]
+          ssl = room_options[:ssl] || campfire_options[:ssl]
+          ssl_verify = room_options[:ssl_verify] || campfire_options[:ssl_verify]
+          room_name = room_options[:room] || campfire_options[:room]
+
+
+          campfire = ::Tinder::Campfire.new account,
+            :token => token,
+            :ssl => ssl,
+            :ssl_verify => ssl_verify
+          campfire.find_room_by_name(room_name)
+        end
+      end
+
+      def speak(message)
+        @rooms.each do |room|
+          room.speak message
+        end
+      end
+    end
+
     def self.extended(configuration)
       configuration.load do
         set :campfire_options, {}
@@ -21,6 +48,10 @@ module Capistrano
             :ssl_verify => ssl_verify
 
           campfire.find_room_by_name(room_name)
+        end
+
+        set :campfire_rooms do
+          RoomCollection.new(campfire_options)
         end
       end
     end
--- spec/capistrano-campfire_spec.rb
+++ spec/capistrano-campfire_spec.rb
@@ -16,22 +16,68 @@ describe Capistrano::Campfire do
       configuration.campfire_options.should == {}
     end
 
-    it "gives access to a room, given correct campfire options" do
-      configuration.set :campfire_options, {
-        :account => "awesomellc",
-        :token => "yyz123",
-        :ssl => true,
-        :room => "General Awesomeness"
-      }
+    context "with configuration for a single room" do
+      before do
+        configuration.set :campfire_options, {
+          :account => "awesomellc",
+          :token => "yyz123",
+          :ssl => true,
+          :room => "General Awesomeness"
+        }
+      end
 
-      campfire = stub("campfire")
-      ::Tinder::Campfire.should_receive(:new).with("awesomellc", :token => "yyz123", :ssl => true, :ssl_verify => nil).and_return(campfire)
+      it "sends messages to single room using `campfire_room.speak`" do
+        campfire = stub("campfire")
+        ::Tinder::Campfire.should_receive(:new).with("awesomellc", :token => "yyz123", :ssl => true, :ssl_verify => nil).and_return(campfire)
 
-      room = stub("room")
-      campfire.should_receive(:find_room_by_name).with("General Awesomeness").and_return(room)
+        room = stub("room")
+        campfire.should_receive(:find_room_by_name).with("General Awesomeness").and_return(room)
 
-      configuration.campfire_room.should == room
+        room.should_receive(:speak).with("IMPENDING DOOM")
+
+        configuration.campfire_room.speak "IMPENDING DOOM"
+      end
+
     end
+
+    context "with configuration for multiple rooms" do
+      before do
+        configuration.set :campfire_options, :rooms => [{
+                                                         :account => 'zim',
+                                                         :room => 'World Conquest',
+                                                         :token => '001000101110101001011112'
+                                                       }, {
+                                                         :account => 'swolleneyeballnetwork',
+                                                         :room => 'Agents',
+                                                         :token => '2001000101110101001011110'
+                                                       }],
+                                             :ssl => true
+      end
+
+      it "sends messages to all rooms using `campfire_rooms.speak`" do
+        zim_campfire = stub("zim_campfire")
+        ::Tinder::Campfire.should_receive(:new).with('zim', :token => '001000101110101001011112', :ssl => true, :ssl_verify => nil).and_return(zim_campfire)
+
+
+        world_conquest_room = stub("world_conquest_room")
+        zim_campfire.should_receive(:find_room_by_name).with("World Conquest").and_return(world_conquest_room)
+
+
+        swolleneyeballnetwork_campfire = stub("swolleneyeballnetwork_campfire")
+        ::Tinder::Campfire.should_receive(:new).with('swolleneyeballnetwork', :token => '2001000101110101001011110', :ssl => true, :ssl_verify => nil).and_return(swolleneyeballnetwork_campfire)
+
+
+        agents_room = stub("agents_room")
+        swolleneyeballnetwork_campfire.should_receive(:find_room_by_name).with("Agents").and_return(agents_room)
+
+
+        world_conquest_room.should_receive(:speak).with("DOOM IMPENDING")
+        agents_room.should_receive(:speak).with("DOOM IMPENDING")
+
+        configuration.campfire_rooms.speak "DOOM IMPENDING"
+      end
+    end
+
 
   end
 
